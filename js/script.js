4176cb6a236670a6a4a93da928290eb33747ea94
saifulislam-saif <msi132043@gmail.com>
2020-06-15 18:39:42 +0600

    Home Page Design

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Shorthand for $( document ).ready()
+$(function () {
+
+    //Student Counter
+    $('.counter').counterUp({
+        delay: 19,
+        time: 1919
+    });
+
+
+});
+
+
+//Logo Typing Effect Create By MSI:313
+msiTyping("logo_typing", 200);
+
+
+//Mix Available Batch
+var containerEl = document.querySelector('.schedule_table');
+var mixer = mixitup(containerEl);
