d40c17b32b6fbdfeb3ea06cc058263a089d70325
msi132043@gmail.com <msi132043@gmail.com>
2020-06-16 20:07:23 +0600

    Home Page Design : JavaScript Effect

--- js/msi-typing.js
+++ js/msi-typing.js
@@ -1,18 +1,21 @@
-msiTyping = (id, speed = 300) => {
-var selector = document.getElementById(id);
-var txt = selector.innerHTML;
-selector.innerHTML = "";
-var i = 0;
-(function msi() {
-  if (i < txt.length) {
-    selector.innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(msi, speed);
-  }
-  else {
-  i = 0;
+msiTyping = (id, speed = 300, multi = 0) => {
+  var selector = document.querySelector(id);
+  var txt = selector.innerText;
   selector.innerHTML = "";
-  setTimeout(msi, speed);
- }
-})();
+  var i = 0;
+  (function msi() {
+    if (i < txt.length) {
+      if (txt.charAt(i)=="|" && multi == 1) {
+        selector.innerHTML = "";
+      } else {
+        selector.innerHTML += txt.charAt(i);
+      }
+      i++;
+      setTimeout(msi, speed);
+    } else {
+      i = 0;
+      selector.innerHTML = "";
+      setTimeout(msi, speed);
+    }
+  })();
 }
