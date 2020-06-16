200c15410b4163d59676c196d9492a91fcd6923a
saifulislam-saif <msi132043@gmail.com>
2020-06-16 13:03:43 +0600

    Home Page Design with slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
+// ================ JavaScript ================
+
+//Logo Typing Effect Create By MSI:313
+msiTyping("logo_typing", 200);
+
+
+//Mix Available Batch
+var containerEl = document.querySelector('.schedule_table');
+var mixer = mixitup(containerEl);
+
+
+
+// ================ jQuery Start ================
+
 // Shorthand for $( document ).ready()
 $(function () {
 
@@ -7,14 +21,39 @@ $(function () {
         time: 1919
     });
 
+    //Testimonial Slider
+    $('.testimonial_slider').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: true,
+
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+  ]
+    });
+
 
 });
-
-
-//Logo Typing Effect Create By MSI:313
-msiTyping("logo_typing", 200);
-
-
-//Mix Available Batch
-var containerEl = document.querySelector('.schedule_table');
-var mixer = mixitup(containerEl);
